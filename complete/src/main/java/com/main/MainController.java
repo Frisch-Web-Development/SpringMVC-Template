44a11e5c0fea1ee20949297c9d59426164292049
Gavri Kepets <gkepets@gmail.com>
2017-09-06 15:20:47 -0400

    Working On Google+ Auth

--- complete/src/main/java/com/main/MainController.java
+++ complete/src/main/java/com/main/MainController.java
@@ -3,6 +3,7 @@ package com.main;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -11,11 +12,15 @@ public class MainController {
 	public String redirect(Model model) {
 		return "redirect:/home";
 	}
-	
-	
+
+	@PostMapping("/login")
+	public String postHome(Model model) {
+		model.addAttribute("home", new Entity());
+		return "redirect:/home";
+	}
+
 	@GetMapping("/home")
 	public String getHome(Model model) {
-		model.addAttribute("home", new Entity());
 		return "home";
 	}
 
